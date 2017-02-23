66aeff2f39902353821c49dabbf181eab8c93063
Ashish <akjha992@gmail.com>
2017-02-23 23:56:46 +0530

    module4

---
+++ Module4/script.js
@@ -0,0 +1,13 @@
+(function script(window) {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var i=0;i<10;i++) {
+    var firstLetter=names[i].charAt(0);
+  if (firstLetter.toLowerCase()=='j') {
+
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+
+}
+})(window);
---
+++ java/js/script.js
@@ -0,0 +1,7 @@
+var x=5;
+var y=4
+function myfun(x,y) {
+	return x>y;
+}
+myfun();
+console.log(myfun);
